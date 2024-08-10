d65b85c7c7d8c66caf1c2762409c7ba2e0d0336d
NagarajKR <nagarajkrkatahrea@gmail.com>
2024-08-11 05:07:42 +0530

    2 line added to comapre

--- src/Programing/cartpage.java
+++ src/Programing/cartpage.java
@@ -5,7 +5,12 @@ public class cartpage {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
+		System.out.println("login");
+		
 		System.out.println("cart page added successfully");
+		System.out.println("added succssefully");
+		System.out.println("Logout successfully");
 
 	}
 
