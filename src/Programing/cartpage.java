618a690bd7376cec34c141b6701e79a32635f5cd
NagarajKR <nagarajkrkatahrea@gmail.com>
2024-08-10 20:27:12 +0530

    cart added

---
+++ src/Programing/cartpage.java
@@ -0,0 +1,12 @@
+package Programing;
+
+public class cartpage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("cart page added successfully");
+
+	}
+
+}
